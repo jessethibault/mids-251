473342880ed9a81d4bce44944ad9ef23eb4dff85
Jesse Thibault <jethibau@ischool.berkeley.edu>
2021-09-12 13:53:06 -0700

    Create message forwarder

--- hw3/message-forwarder/forward.py
+++ hw3/message-forwarder/forward.py
@@ -1,11 +1,10 @@
 import paho.mqtt.client as mqtt
 
-
 LOCAL_MQTT_HOST="mosquitto-service"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="image_capture"
 
-REMOTE_MQTT_HOST="blah"
+REMOTE_MQTT_HOST="mosquitto-service"
 REMOTE_MQTT_PORT=1883
 REMOTE_MQTT_TOPIC="image_store"
 
@@ -13,12 +12,13 @@ def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
         client.subscribe(LOCAL_MQTT_TOPIC)
 	
+def on_connect_remote(client, userdata, flags, rc):
+        print("connected to remote broker with rc: " + str(rc))
+
 def on_message(client,userdata, msg):
   try:
-    print("message received: ",str(msg.payload.decode("utf-8")))
-    # if we wanted to re-publish this message, something like this should work
-    # msg = msg.payload
-    # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+    msg = msg.payload
+    remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
@@ -27,7 +27,9 @@ local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 local_mqttclient.on_message = on_message
 
-
+remote_mqttclient = mqtt.Client()
+remote_mqttclient.on_connect = on_connect_remote
+remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
 
 # go into a loop
 local_mqttclient.loop_forever()
