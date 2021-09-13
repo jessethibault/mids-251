07e9ae80e288f6f0fce1182b654c10ee2735b746
Jesse Thibault <jethibau@ischool.berkeley.edu>
2021-09-12 23:41:34 -0700

    Add boto3 code to upload file to S3

--- hw3/image-processor/image_processor.py
+++ hw3/image-processor/image_processor.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import sys
 import time
+import boto3
 
 LOCAL_MQTT_HOST="mosquitto-service"
 LOCAL_MQTT_PORT=1883
@@ -9,11 +10,19 @@ LOCAL_MQTT_TOPIC="image_store"
 def on_connect_local(client, userdata, flags, rc):
   print(f"connected to local broker with rc: {rc}")
   client.subscribe(LOCAL_MQTT_TOPIC)
-	
+
+# Keep for debugging purposes
 def write_to_file(payload):
   f = open(f"{round(time.time() * 1000000)}.png", "wb")
   f.write(payload)
   f.close()
+
+# Write payload to S3 using boto3
+def write_to_s3(payload):
+  client = boto3.client('s3')
+  resp = client.put_object(ACL='public-read', Body=payload, Bucket='hw3-faces', Key=f"{round(time.time() * 1000000)}.png")
+  if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
+    print(f"Error pushing file: {resp}")
 
 def on_message(client,userdata, msg):
   try:
