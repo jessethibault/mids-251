891d353b4a50ff196b505ccd53f5814dfa6dc731
Jesse Thibault <jethibau@ischool.berkeley.edu>
2021-09-13 00:28:03 -0700

    Add debug logging..

--- hw3/image-processor/image_processor.py
+++ hw3/image-processor/image_processor.py
@@ -19,13 +19,17 @@ def write_to_file(payload):
 
 # Write payload to S3 using boto3
 def write_to_s3(payload):
+  print('write_to_s3 start')
   resp = boto_client.put_object(ACL='public-read', Body=payload, Bucket='hw3-faces', Key=f"{round(time.time() * 1000000)}.png")
   if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
     print(f"Error pushing file: {resp}")
+  print('write_to_s3 end')
 
 def on_message(client,userdata, msg):
   try:
+    print('on_message start')
     write_to_s3(msg.payload)
+    print('on_message end')
   except:
     print(f"Unexpected error: {sys.exc_info()[0]} -- {sys.exc_info()[1]} -- {sys.exc_info()[2]}")
 
@@ -34,6 +38,6 @@ local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 local_mqttclient.on_message = on_message
 boto_client = boto3.client('s3')
-
+print('startup even working?')
 # go into a loop
 local_mqttclient.loop_forever()
