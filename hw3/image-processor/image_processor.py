80399025526818dac9f082e1e12ff0e2bcaf6789
Jesse Thibault <jethibau@ischool.berkeley.edu>
2021-09-13 01:01:33 -0700

    Revert all boto3 work

--- hw3/image-processor/image_processor.py
+++ hw3/image-processor/image_processor.py
@@ -1,7 +1,6 @@
 import paho.mqtt.client as mqtt
 import sys
 import time
-import boto3
 
 # Static data defining local host
 LOCAL_MQTT_HOST="mosquitto-service"
@@ -19,18 +18,9 @@ def write_to_file(payload):
   f.write(payload)
   f.close()
 
-# Write payload to S3 using boto3
-def write_to_s3(payload):
-  print('write_to_s3 start')
-  resp = boto_client.put_object(ACL='public-read', Body=payload, Bucket='hw3-faces', Key=f"{round(time.time() * 1000000)}.png")
-  if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
-    print(f"Error pushing file: {resp}")
-  print('write_to_s3 end')
-
 # Run whenever a new message arrives -- a new image
 def on_message(client,userdata, msg):
   try:
-    print('on_message start')
     write_to_file(msg.payload)
   except:
     print(f"Unexpected error: {sys.exc_info()[0]} -- {sys.exc_info()[1]} -- {sys.exc_info()[2]}")
@@ -40,9 +30,6 @@ local_mqttclient = mqtt.Client()
 local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 local_mqttclient.on_message = on_message
-
-# Boto3 client for S3 access
-boto_client = boto3.client('s3')
 
 # go into a loop
 local_mqttclient.loop_forever()
