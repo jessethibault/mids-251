097380ccb1eb3378a150378b810738294721f8ef
jessethibault <littlet102030@hotmail.com>
2021-11-14 21:01:18 -0800

    Upload hw5

---
+++ hw5/trainprep.ps1
@@ -0,0 +1,18 @@
+﻿$items = Get-ChildItem "R:\hw5\train" -Filter *.tar
+
+foreach ($item in $items)
+{
+    $file = Get-Item $item.FullName
+    $newLoc = $item.DirectoryName + "\" + $item.BaseName
+    $newFile = $newLoc + "\" + $item.Name
+
+    New-Item -Path $item.DirectoryName -Name $item.BaseName -ItemType "directory"
+
+    Move-Item -Path $item.FullName -Destination $newFile
+
+    cd $newLoc
+
+    tar -xf $newFile
+
+    Remove-Item $newFile
+}
