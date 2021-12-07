10c9e1cd44f4c2358c1e12b3794648e0a69553a8
Jesse Thibault <jethibau@ischool.berkeley.edu>
2021-12-07 06:16:34 -0800

    add paho-mqtt to remote

--- final/piece-identifier/piece-identifier.py
+++ final/piece-identifier/piece-identifier.py
@@ -1,9 +1,40 @@
-#import keras
+import paho.mqtt.client as mqtt
+import sys
 import time
 
-#model = keras.models.load_model('/models')
+# Static data defining local host
+LOCAL_MQTT_HOST="mosquitto-service"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="image_store"
 
-print('test')
+# Run when connected to local MQTT broker
+def on_connect_local(client, userdata, flags, rc):
+  print(f"connected to local broker with rc: {rc}")
+  client.subscribe(LOCAL_MQTT_TOPIC)
 
-while(True):
-    time.sleep(1)
+# Keep for debugging purposes
+def write_to_file(payload):
+  f = open(f"{round(time.time() * 1000000)}.png", "wb")
+  f.write(payload)
+  f.close()
+
+# Write payload to S3 using boto3
+def make_prediction(payload):
+  # do model pred here
+  print('tbd')
+
+# Run whenever a new message arrives -- a new image
+def on_message(client,userdata, msg):
+  try:
+    make_prediction(msg.payload)
+  except:
+    print(f"Unexpected error: {sys.exc_info()[0]} -- {sys.exc_info()[1]} -- {sys.exc_info()[2]}")
+
+# Build up local MQTT client
+local_mqttclient = mqtt.Client()
+local_mqttclient.on_connect = on_connect_local
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+local_mqttclient.on_message = on_message
+
+# go into a loop
+local_mqttclient.loop_forever()
